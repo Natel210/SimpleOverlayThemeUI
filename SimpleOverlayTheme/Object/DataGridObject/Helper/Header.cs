57ad341bb94d5debdf563a67326d46ac66227102
Natel <51104656+Natel210@users.noreply.github.com>
2024-10-25 18:01:19 +0900

    -

---
+++ SimpleOverlayTheme/Object/DataGridObject/Helper/Header.cs
@@ -0,0 +1,42 @@
+﻿using SimpleOverlayTheme.Object.ComboBoxObject.Helper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace SimpleOverlayTheme.Object.DataGridObject.Helper
+{
+    public class Header
+    {
+        private static readonly FrameworkPropertyMetadataOptions _frameworkPropertyMetadataOptions = FrameworkPropertyMetadataOptions.AffectsRender | FrameworkPropertyMetadataOptions.AffectsMeasure;
+
+        #region ContentPadding
+
+        public static readonly DependencyProperty ContentPaddingProperty
+            = DependencyProperty.RegisterAttached(
+                "ContentPadding",
+                typeof(Thickness),
+                typeof(Header),
+                new FrameworkPropertyMetadata(new Thickness(0), _frameworkPropertyMetadataOptions));
+
+        [Category($"{KeywordDictionary.CategoryKey.Header}")]
+        [AttachedPropertyBrowsableForType(typeof(DataGridColumnHeader))]
+        public static Thickness GetContentPadding(UIElement element)
+        {
+            return (Thickness)element.GetValue(ContentPaddingProperty);
+        }
+
+        [Category($"{KeywordDictionary.CategoryKey.Header}")]
+        [AttachedPropertyBrowsableForType(typeof(DataGridColumnHeader))]
+        public static void SetContentPadding(UIElement element, Thickness value)
+        {
+            element.SetValue(ContentPaddingProperty, value);
+        }
+        #endregion
+    }
+}
--- SimpleOverlayTheme/Object/WindowHeaderObject/Behavior/MaximizeRestoreBehavior.cs
+++ SimpleOverlayTheme/Object/WindowHeaderObject/Behavior/MaximizeRestoreBehavior.cs
@@ -14,6 +14,13 @@ namespace SimpleOverlayTheme.Object.WindowHeaderObject.Behavior
             base.OnAttached();
             AssociatedObject.Checked += OnChecked;
             AssociatedObject.Unchecked += OnUnchecked;
+
+            if (Window.GetWindow(AssociatedObject) is Window window)
+            {
+                window.StateChanged += OnWindowStateChanged;
+                UpdateButtonState(window); // 초기 상태 설정
+            }
+
         }
 
         protected override void OnDetaching()
@@ -35,6 +42,37 @@ namespace SimpleOverlayTheme.Object.WindowHeaderObject.Behavior
         {
             if (Window.GetWindow(AssociatedObject) is Window window)
                 window.WindowState = WindowState.Normal;
+        }
+
+        private void OnWindowStateChanged(object? sender, EventArgs e)
+        {
+            var window = sender as Window;
+            if (window is null)
+                return;
+
+            UpdateButtonState(window);
+            bool isNoneWindowStyle = window.WindowStyle == WindowStyle.None;
+            bool canResize = window.ResizeMode == ResizeMode.CanResizeWithGrip
+                || window.ResizeMode == ResizeMode.CanResize;
+
+            if (isNoneWindowStyle && canResize)
+            {
+                if (window.WindowState == WindowState.Maximized)
+                {
+                    window.Margin = new Thickness(10); // 윈도우 크기 조정 시 추가할 패딩
+                                                       //window.Margin = new Thickness(8);  // 기본 마진으로 조정
+                }
+                else
+                {
+                    window.Margin = new Thickness(0); // 윈도우 크기 조정 시 추가할 패딩
+                                                       //window.Margin = new Thickness(0);  // 기본 마진으로 조정
+                }
+            }
+        }
+
+        private void UpdateButtonState(Window window)
+        {
+            AssociatedObject.SetCurrentValue(ToggleButton.IsCheckedProperty, window.WindowState == WindowState.Maximized);
         }
     }
 }
